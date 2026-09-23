429cb3039764cd381d180bf7ec5f33ae9f00c1ef
ashish27190-svg <ashish.27190@gmail.com>
2026-09-23 10:28:46 +0530

    Refresh Daily Intelligence PWA cache

--- site/daily-intelligence/sw.js
+++ site/daily-intelligence/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'daily-intelligence-v0.6-shell-1';
+const CACHE = 'daily-intelligence-v0.7-market';
 const SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
