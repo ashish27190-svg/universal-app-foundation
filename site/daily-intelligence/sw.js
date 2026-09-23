4d399927d85b07aea6b0aabe008839e4aaaf5e6d
ashish27190-svg <ashish.27190@gmail.com>
2026-09-23 10:20:58 +0530

    chore: refresh Daily Intelligence cache version

--- site/daily-intelligence/sw.js
+++ site/daily-intelligence/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'daily-intelligence-v0.4-shell-1';
+const CACHE = 'daily-intelligence-v0.6-shell-1';
 const SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
