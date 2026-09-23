e83a2b90f453765aa0938675f51c9081253ffb81
ashish27190-svg <ashish.27190@gmail.com>
2026-09-23 10:48:13 +0530

    Refresh Daily Intelligence fast-build cache

--- site/daily-intelligence/sw.js
+++ site/daily-intelligence/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'daily-intelligence-v0.7.1-market';
+const CACHE = 'daily-intelligence-v0.7.2-fast';
 const SHELL = ['./', './index.html', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
